fb96a7ec839127d5af7836cf1082117fc82c154d
ekrmdgr <ekremdeger@outlook.com>
2020-11-22 23:00:33 +0300

    Add project files.

---
+++ MyWebApi/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MyWebApi
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+
+    public class User {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ MyWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly CatalogContext _catalogContext;
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, CatalogContext catalogContext)
+        {
+            _logger = logger;
+            _catalogContext = catalogContext;
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _catalogContext.Users.ToList();
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var users = _catalogContext.Users.ToList();
+            return "webapi (with value " + users.FirstOrDefault().FullName + ")";
+        }
+
+    }
+}
